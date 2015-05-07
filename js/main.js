fb7a10365d30f96e221b12343ca4e3982a8fe098
umar432 <umar432@gmail.com>
2015-05-07 18:20:47 +0200

    eve

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
    
 	$('tr').click(function(event) {
+		
         $('tr').not(this).removeClass('clicked');
-	
-        $(this).toggleClass('clicked'); 
+		$(this).toggleClass('clicked'); 
 		
 		
     });
