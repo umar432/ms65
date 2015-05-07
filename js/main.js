01339bb38e46ac5045a91473e947b8c3553a527b
umar432 <umar432@gmail.com>
2015-05-07 16:58:44 +0200

    Version 2

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+   
+	$('tr').click(function(event) {
+        $('tr').not(this).removeClass('clicked');
+	
+        $(this).toggleClass('clicked'); 
+		
+		
+    });
+});
