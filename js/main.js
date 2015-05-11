806722f080639436d58be308c3ce00ff64bc47cb
umar432 <umar432@gmail.com>
2015-05-11 18:55:11 +0200

    Updated Evening

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() 
+{
+    $("#cont").click(function() 
+	{
+		
+	window.location.href = "page2.html";	
+	
+	
+	});
+	 $("#page2").click(function() 
+	{
+		
+	window.location.href = "page3.html";	
+	
+	
+	});
+	
+	 $("#page3").click(function() 
+	{
+		
+	window.location.href = "page4.html";	
+	
+	
+	});
+		 $("#page4").click(function() 
+	{
+		
+	window.location.href = "page5.html";	
+	
+	
+	});
+	
+		 $("#page5").click(function() 
+	{
+		
+	window.location.href = "index.html";	
+	
+	
+	});
+	
+});
