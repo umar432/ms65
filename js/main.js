a5bc06fca218e6b7771c7ff7011197b20f205a0a
umar432 <umar432@gmail.com>
2015-05-12 10:35:35 +0200

    update

--- js/main.js
+++ js/main.js
@@ -38,4 +38,54 @@ $(document).ready(function()
 	
 	});
 	
+	$("#btnShowSimple").click(function (e)
+      {
+         ShowDialog(false);
+         e.preventDefault();
+      });
+
+      $("#btnShowModal").click(function (e)
+      {
+         ShowDialog(true);
+         e.preventDefault();
+      });
+
+      $("#btnClose").click(function (e)
+      {
+         HideDialog();
+         e.preventDefault();
+      });
+
+      $("#btnSubmit").click(function (e)
+      {
+         var brand = $("#brands input:radio:checked").val();
+         $("#output").html("<b>Your favorite mobile brand: </b>" + brand);
+         HideDialog();
+         e.preventDefault();
+      });
+	
 });
+
+function ShowDialog(modal)
+   {
+      $("#overlay").show();
+      $("#dialog").fadeIn(300);
+
+      if (modal)
+      {
+         $("#overlay").unbind("click");
+      }
+      else
+      {
+         $("#overlay").click(function (e)
+         {
+            HideDialog();
+         });
+      }
+   }
+
+   function HideDialog()
+   {
+      $("#overlay").hide();
+      $("#dialog").fadeOut(300);
+   } 
