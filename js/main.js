7c70358ec378c03fd535ec55d816b70b1d6b489e
umar432 <umar432@gmail.com>
2015-05-20 14:48:25 +0200

    ups

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function() 
 {
 	$('.hdr-logo').click(function(e) { window.location.href = "index.html";});
-	$('#home').click(function(e) {jQuery(this).addClass('hact'); window.location.href = "page2.html";});
+	$('#home').click(function(e) {jQuery(this).addClass('hact'); window.location.href = "page3.html";});
 	$('#btnR').click(function(e) { window.location.href = "index.html";});
-	$(".tab-er").click(function (e)  {  ShowDialog(false); e.preventDefault();  });
-
+	$('#btnV').click(function(e) { window.location.href = "page3.html";});
+	$(".tab-er").click(function (e)  {  ShowDialogEr(false); e.preventDefault();  });
+	$(".web_Hrht").click(function (e) { HideDialog(); e.preventDefault();  });
    
 	
 	
@@ -25,7 +26,7 @@ $(document).ready(function()
 	{
 		
 		jQuery('.accordion .accordion-section-title').removeClass('active');
-		jQuery('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+		jQuery('.accordion .accordion-section-content').slideUp(100).removeClass('open');
 	}
 
 	jQuery('.accordion-section-title').click(function(e) {
@@ -42,7 +43,7 @@ $(document).ready(function()
 			// Add active class to section title
 			jQuery(this).addClass('active');
 			// Open up the hidden content panel
-			jQuery('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+			jQuery('.accordion ' + currentAttrValue).slideDown(100).addClass('open'); 
 			
 		}
 
@@ -59,3 +60,12 @@ function ShowDialog(modal)  {   $("#overlay").show();   $("#dialog").fadeIn(300)
    }
 
    function HideDialog() { $("#overlay").hide(); $("#dialog").fadeOut(300);  } 
+   
+function ShowDialogEr(modal)  {   $("#overlay").show();   $("#dailogEr").fadeIn(300);
+
+      if (modal) { $("#overlay").unbind("click");  }
+      else { $("#overlay").click(function (e) { HideDialog(); });
+      }
+   }
+
+   function HideDialog() { $("#overlay").hide(); $("#dailogEr").fadeOut(300);  } 
