40899713be033005e3ab915221c2e9e0c8392a56
umar432 <umar432@gmail.com>
2015-05-20 13:13:58 +0200

    yes

--- js/main.js
+++ js/main.js
@@ -1,71 +1,28 @@
 $(document).ready(function() 
 {
-    $("#cont").click(function() 
-	{
-		
-	window.location.href = "page2.html";	
-	
-	
-	});
-	 $("#page2").click(function() 
-	{
-		
-	window.location.href = "page3.html";	
-	
-	
-	});
-	
-	 $("#page3").click(function() 
-	{
-		
-	window.location.href = "page4.html";	
-	
-	
-	});
-		 $("#page4").click(function() 
-	{
-		
-	window.location.href = "page5.html";	
-	
-	
-	});
-	
-		 $("#page5").click(function() 
-	{
-		
-	window.location.href = "index.html";	
-	
-	
-	});
-	
-	$("#btnShowSimple").click(function (e)
-      {
-         ShowDialog(false);
-         e.preventDefault();
-      });
+	$('.hdr-logo').click(function(e) { window.location.href = "index.html";});
+	$('#home').click(function(e) {jQuery(this).addClass('hact'); window.location.href = "page2.html";});
+	$('#btnR').click(function(e) { window.location.href = "index.html";});
+	$(".tab-er").click(function (e)  {  ShowDialog(false); e.preventDefault();  });
 
-      $("#btnShowModal").click(function (e)
+   
+	
+	
+	$("#btnShowSimple").click(function (e)  {  ShowDialog(false); e.preventDefault();  });
+    $("#btnShowModal").click(function (e)  { ShowDialog(true);  e.preventDefault();   });
+    $("#btnClose").click(function (e) { HideDialog(); e.preventDefault();  });
+    $("#btnSubmit").click(function (e)
       {
-         ShowDialog(true);
-         e.preventDefault();
-      });
-
-      $("#btnClose").click(function (e)
-      {
-         HideDialog();
-         e.preventDefault();
-      });
-
-      $("#btnSubmit").click(function (e)
-      {
-         var brand = $("#brands input:radio:checked").val();
-         $("#output").html("<b>Your favorite mobile brand: </b>" + brand);
+         /*var brand = $("#brands input:radio:checked").val();
+         $("#output").html("<b>Your favorite mobile brand: </b>" + brand);*/
+		 window.location.href = "login.html";
          HideDialog();
          e.preventDefault();
       });
 	  
 	  
-	function close_accordion_section() {
+	function close_accordion_section()
+	{
 		
 		jQuery('.accordion .accordion-section-title').removeClass('active');
 		jQuery('.accordion .accordion-section-content').slideUp(300).removeClass('open');
@@ -94,26 +51,11 @@ $(document).ready(function()
 	
 });
 
-function ShowDialog(modal)
-   {
-      $("#overlay").show();
-      $("#dialog").fadeIn(300);
+function ShowDialog(modal)  {   $("#overlay").show();   $("#dialog").fadeIn(300);
 
-      if (modal)
-      {
-         $("#overlay").unbind("click");
-      }
-      else
-      {
-         $("#overlay").click(function (e)
-         {
-            HideDialog();
-         });
+      if (modal) { $("#overlay").unbind("click");  }
+      else { $("#overlay").click(function (e) { HideDialog(); });
       }
    }
 
-   function HideDialog()
-   {
-      $("#overlay").hide();
-      $("#dialog").fadeOut(300);
-   } 
+   function HideDialog() { $("#overlay").hide(); $("#dialog").fadeOut(300);  } 
